0f6bb2c03e7364ae3f42e77ad8e19277354a36ea
Andrew Wise <andrew@andrewwise.co.uk>
2021-04-30 20:33:54 +0100

    Countdown messages improved

--- bot.rb
+++ bot.rb
@@ -19,7 +19,7 @@ end
 
 gif_words = ["gif","Gif","GIF"]
 
-bot.mention(containing: "gif") do |event|
+bot.mention(containing: gif_words) do |event|
   event.channel.send_file File.new(Dir.glob("data/gifs/*.gif").sample)
 end
 
@@ -29,7 +29,16 @@ bot.mention(containing: not!(trivia_words + gif_words)) do |event|
 
   daystoeurovision = eurovision.mjd - now.mjd
 
-  event.respond "#{daystoeurovision} days to the Eurovision final!"
+  case daystoeurovision
+  when -1
+    event.respond "Anyone else hungover?"
+  when 0
+    event.respond "TODAY IS THE DAY!"
+  when 1
+    event.respond "Tomorrow is the Eurovision final!"
+  when 2..30
+    event.respond "#{daystoeurovision} days to the Eurovision final!"
+  end
 end
 
 bot.run
--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,17 @@ bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
 now = Date.today
 eurovision = Date.new(2021,5,22)
 daystoeurovision = eurovision.mjd - now.mjd
-message = "Good morning! It's #{daystoeurovision} days to the Eurovision final!"
+
+case daystoeurovision
+when -1
+  message = "Anyone else hungover?"
+when 0
+  message = "TODAY IS THE DAY!"
+when 1
+  message = "Tomorrow is the Eurovision final!"
+when 2..30
+  message = "Good morning! It's #{daystoeurovision} days to the Eurovision final!"
+end
 
 bot.run :async
 
